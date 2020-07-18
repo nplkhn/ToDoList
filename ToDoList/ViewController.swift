a116b5384894ad2a8bec2c244517906ebbffdca1
Nikita Plakhin <nplkhn1@yandex.ru>
2020-07-18 11:45:15 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Никита Плахин on 7/18/20.
+//  Copyright © 2020 Никита Плахин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
